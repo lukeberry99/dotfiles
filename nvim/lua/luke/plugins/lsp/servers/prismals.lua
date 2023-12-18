a46b0993a60bc59a87fb6851e1bfcaf4d96c8d36
Luke Berry <lukeberry99@gmail.com>
2023-12-18 18:37:45 +0000

    lmao starting again

--- nvim/lua/luke/plugins/lsp/servers/prismals.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-local lspconfig = require("lspconfig")
-
-M.setup = function(opts)
-  lspconfig.prismals.setup({})
-end
-
-return M
--- nvim/lua/luke/plugins/lsp/servers/tailwindcss.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-local lspconfig = require("lspconfig")
-
-M.setup = function(opts)
-  lspconfig.tailwindcss.setup({})
-end
-
-return M
