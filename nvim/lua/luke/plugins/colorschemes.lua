1b5ee73781c50738b4e7bc33338f897bafb17317
Luke Berry <lukeberry99@gmail.com>
2024-01-04 14:13:13 +0000

    config

--- nvim/lua/luke/plugins/colorschemes.lua
+++ nvim/lua/luke/plugins/colorschemes.lua
@@ -1,13 +1,13 @@
 return {
 	{ "shaunsingh/oxocarbon.nvim" },
 	{ "ellisonleao/gruvbox.nvim" },
-	{
-		"rebelot/kanagawa.nvim",
-		priority = 1000,
-		lazy = false,
-	},
+	{ "rebelot/kanagawa.nvim" },
 	{
 		"rose-pine/neovim",
 		name = "rose-pine",
+		priority = 1000, -- high prio because we want it quiiiiiick
+		config = function()
+			vim.cmd("colorscheme rose-pine-moon")
+		end,
 	},
 }
--- nvim/lua/luke/prefs.lua
+++ nvim/lua/luke/prefs.lua
@@ -1,6 +1,5 @@
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
-vim.cmd("colorscheme rose-pine-moon")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
