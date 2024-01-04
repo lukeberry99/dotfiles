04f28ad8d2eef463e94920c87e5e36011e0f4381
Luke Berry <lukeberry99@gmail.com>
2024-01-04 13:07:36 +0000

    add dressing

---
+++ nvim/lua/plugins/dressing.lua
@@ -0,0 +1,13 @@
+return {
+	"stevearc/dressing.nvim",
+	lazy = true,
+	config = function()
+		require("dressing").setup({
+			input = {
+				win_options = {
+					winhighlight = "NormalFloat:DiagnosticError",
+				},
+			},
+		})
+	end,
+}
