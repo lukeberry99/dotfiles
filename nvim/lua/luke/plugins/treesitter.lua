e10bfb426e3d74f1a9b9c66c8ca51b499c3eec9f
Luke Berry <lukeberry99@gmail.com>
2024-01-04 14:26:58 +0000

    remove silly config

--- nvim/lua/luke/plugins/treesitter.lua
+++ nvim/lua/luke/plugins/treesitter.lua
@@ -2,42 +2,8 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	dependencies = {
-		-- Allows to define text objects based on treesitter
 		"nvim-treesitter/nvim-treesitter-textobjects",
-		-- Allows to define which comments to use dinamically (make sure to set the comment plugin integration!)
 		"JoosepAlviste/nvim-ts-context-commentstring",
-		-- Auto close/rename html tags
 		"windwp/nvim-ts-autotag",
 	},
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-			modules = {},
-			sync_install = false,
-			ignore_install = {},
-			ensure_installed = { "markdown_inline", "regex" },
-			auto_install = true,
-			matchup = {
-				enable = false,
-			},
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-				disable = { "bash" },
-			},
-			indent = { enable = false },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<c-space>",
-					node_incremental = "<c-space>",
-					scope_incremental = "<c-s>",
-					node_decremental = "<M-space>",
-				},
-			},
-			autotag = {
-				enable = true,
-			},
-		})
-	end,
 }
