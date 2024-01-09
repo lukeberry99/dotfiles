8e849f2dfd83d01e9bce50172c6d4f84a739c0a8
Luke Berry <lukeberry99@gmail.com>
2024-01-09 20:48:36 +0000

    just some things

--- nvim/lua/luke/plugins/obsidian.lua
+++ nvim/lua/luke/plugins/obsidian.lua
@@ -53,6 +53,9 @@ return {
 			{
 				name = "personal",
 				path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/personal",
+				overrides = {
+					notes_subdir = "03 Commonplace",
+				},
 			},
 			{
 				name = "writing",
@@ -60,6 +63,7 @@ return {
 			},
 		},
 		daily_notes = {
+			folder = "02 Journals",
 			-- Optional, if you want to change the date format for the ID of daily notes.
 			date_format = "%Y-%m-%d",
 			-- Optional, if you want to change the date format of the default alias of daily notes.
