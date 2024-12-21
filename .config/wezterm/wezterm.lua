06918ff1abca2b4e2dcc6b03eda7984d77cff08d
Luke Berry <lukeberry99@gmail.com>
2024-12-21 10:00:16 +0000

    wezterm better now

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,9 +6,6 @@ require("tabs").setup(config)
 require("links").setup(config)
 require("mouse").setup(config)
 
-config.enable_wayland = true
-config.webgpu_power_preference = "HighPerformance"
-
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
 
@@ -24,25 +21,10 @@ config.underline_thickness = 3
 config.cursor_thickness = 4
 config.underline_position = -6
 
-if wezterm.target_triple:find("windows") then
-	config.default_prog = { "pwsh" }
-	config.window_decorations = "RESIZE|TITLE"
-	wezterm.on("gui-startup", function(cmd)
-		local screen = wezterm.gui.screens().active
-		local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
-		local gui = window:gui_window()
-		local width = 0.7 * screen.width
-		local height = 0.7 * screen.height
-		gui:set_inner_size(width, height)
-		gui:set_position((screen.width - width) / 2, (screen.height - height) / 2)
-	end)
-else
-	config.term = "wezterm"
-	config.window_decorations = "NONE"
-end
+-- config.window_decorations = "RESIZE"
 
 -- Fonts
-config.font_size = 10
+config.font_size = 14
 config.font = wezterm.font({ family = "FiraCode Nerd Font" })
 config.bold_brightens_ansi_colors = true
 config.font_rules = {
@@ -67,8 +49,6 @@ config.font_rules = {
 config.default_cursor_style = "BlinkingBar"
 config.force_reverse_video_cursor = true
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
--- window_background_opacity = 0.9,
--- cell_width = 0.9,
 config.scrollback_lines = 10000
 
 -- Command Palette
@@ -77,77 +57,3 @@ config.command_palette_bg_color = "#394b70"
 config.command_palette_fg_color = "#828bb8"
 
 return config
--- local k = require("utils/keys")
--- local wezterm = require("wezterm")
--- local act = wezterm.action
--- --
--- -- local appearance_themes = {
--- -- 	Light = "Rosé Pine Dawn (base16)",
--- -- 	Dark = "Rosé Pine (base16)",
--- -- }
--- --
--- -- local appearance = wezterm.gui.get_appearance()
---
--- local config = {
--- 	color_scheme = "Gruvbox Dark (Gogh)",
--- 	font_size = 10.0,
--- 	font = wezterm.font("Menlo"),
--- 	audible_bell = "Disabled",
--- 	enable_tab_bar = false,
--- 	native_macos_fullscreen_mode = false,
--- 	window_close_confirmation = "NeverPrompt",
--- 	window_background_opacity = 1,
--- 	window_decorations = "RESIZE",
---
--- 	window_padding = {
--- 		left = 0,
--- 		right = 0,
--- 		top = 0,
--- 		bottom = 0,
--- 	},
---
--- 	keys = {
--- 		k.cmd_to_tmux_prefix("1", "1"), -- first window
--- 		k.cmd_to_tmux_prefix("2", "2"),
--- 		k.cmd_to_tmux_prefix("3", "3"),
--- 		k.cmd_to_tmux_prefix("4", "4"),
--- 		k.cmd_to_tmux_prefix("5", "5"),
--- 		k.cmd_to_tmux_prefix("6", "6"),
--- 		k.cmd_to_tmux_prefix("7", "7"),
--- 		k.cmd_to_tmux_prefix("8", "8"),
--- 		k.cmd_to_tmux_prefix("9", "9"), -- last window
---
--- 		k.cmd_to_tmux_prefix("'", "'"), -- last session
--- 		k.cmd_to_tmux_prefix(";", ";"), -- last window
---
--- 		k.cmd_to_tmux_prefix("g", "g"), -- lazygit
--- 		k.cmd_to_tmux_prefix("e", "|"), -- vertical split
--- 		k.cmd_to_tmux_prefix("E", "-"), -- horizontal split
--- 		k.cmd_to_tmux_prefix("o", "u"), -- open url
--- 		k.cmd_to_tmux_prefix("z", "z"), -- zoom window
--- 		k.cmd_to_tmux_prefix("w", "x"), -- kill window
--- 		k.cmd_to_tmux_prefix("t", "c"), -- new window
--- 		k.cmd_to_tmux_prefix("u", "["), -- vim mode
---
--- 		-- t-smart-session-picker
--- 		{
--- 			mods = "CMD",
--- 			key = "k",
--- 			action = act.Multiple({
--- 				act.SendKey({ mods = "CTRL", key = " " }),
--- 				act.SendKey({ key = "T" }),
--- 			}),
--- 		},
---
--- 		-- vim - save file
--- 		k.cmd_key(
--- 			"s",
--- 			act.Multiple({
--- 				act.SendKey({ key = "\x1b" }), -- escape
--- 				k.multiple_actions(":w"),
--- 			})
--- 		),
--- 	},
--- }
---
--- return config
